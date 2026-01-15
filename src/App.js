b77190702f228c34a32dda64bc34216627755981
MaheshAdvaint <maheshb@advaintai.com>
2026-01-15 15:13:52 +0530

    Add Docker image with axios API call

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from "axios";
 
 function App() {
+  const [response, setResponse] = useState('');
+
+
+  useEffect(() => {
+    axios
+      .get("http://192.168.1.191:3000")
+      .then((response) => {
+        console.log("res", response.data);
+        setResponse(response.data);
+      })
+      .catch((err) => {
+        console.error(err.message);
+      });
+  }, []);
+
+
   return (
     <div style={{ textAlign: "center", marginTop: 40 }}>
       <h1>🚀 Product Web UI</h1>
       <p>React containerized and versioned</p>
+      <p>React API With Axios</p>
+      <div>
+        <h3>{'API Response :' + response}</h3>
+      </div>
     </div>
   );
 }
-
 
 export default App;
