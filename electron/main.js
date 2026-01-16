22f6e80b3b2261ca1a81490c1688e28bd59c5075
MaheshAdvaint <maheshb@advaintai.com>
2026-01-16 13:08:58 +0530

    Fix Electron entry point and packaging

--- electron/main.js
+++ electron/main.js
@@ -2,7 +2,7 @@ const { app, BrowserWindow } = require("electron");
 const path = require("path");
 
 function createWindow() {
-  const mainWindow = new BrowserWindow({
+  const win = new BrowserWindow({
     width: 1200,
     height: 800,
     webPreferences: {
@@ -10,12 +10,11 @@ function createWindow() {
     },
   });
 
- const startUrl =
-  process.env.ELECTRON_START_URL ||
-  path.join(__dirname, "../build/index.html");
-
-mainWindow.loadFile(startUrl);
-
+  if (process.env.ELECTRON_START_URL) {
+    win.loadURL(process.env.ELECTRON_START_URL);
+  } else {
+    win.loadFile(path.join(__dirname, "../build/index.html"));
+  }
 }
 
 app.whenReady().then(createWindow);
