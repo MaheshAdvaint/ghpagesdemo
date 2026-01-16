8b927068e650b603343f35aa4f3c541f3323188c
MaheshAdvaint <maheshb@advaintai.com>
2026-01-16 12:05:02 +0530

    Prepare Electron CI

---
+++ electron/main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+ const startUrl =
+  process.env.ELECTRON_START_URL ||
+  path.join(__dirname, "../build/index.html");
+
+mainWindow.loadFile(startUrl);
+
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
